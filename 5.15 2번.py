d4fce4516dc748978ef1ec508a2f2d438b84c0f7
byungkyun <39305703+byungkyun@users.noreply.github.com>
2018-05-16 15:57:37 +0900

    Update 5.15 2번.py

--- 5.15 2번.py
+++ 5.15 2번.py
@@ -1,9 +1,9 @@
 s = ""
 k = ""
-str = input("문자열 : ")
-for i in range (len(str)):
-    s = s + str[i]
-for j in range (len(str)-1 , -1 ,-1):
-    k = k + str[j]
+string = input("문자열 : ")
+for i in range (len(string)):
+    s = s + string[i]
+for j in range (len(string)-1 , -1 ,-1):
+    k = k + string[j]
 print("개별 문자 출력 : " ,s)
 print("역순 개별 문자 출력 : ",k)
